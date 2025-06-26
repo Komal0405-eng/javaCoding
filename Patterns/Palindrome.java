df4a559a19551299d945fbadaa5fb53de1d5662c
Komal <Komalpujari04@gmail.com>
2025-06-26 22:32:34 +0530

    Add JAVACODING Folder

---
+++ Patterns/Palindrome.java
@@ -0,0 +1,25 @@
+import java.util.*;
+ public class Palindrome {
+    public static void palindromic_triangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            //  spaces  
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  "); 
+            }
+            for (int j = i; j >= 1; j++) {
+                System.out.print(j + " ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+              System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter totRows: ");
+        int n = sc.nextInt();
+        palindromic_triangle(n);
+    }
+}   
