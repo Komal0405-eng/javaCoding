cdc8be12f6f3d878f9bbdb8ae44a01ade25acfae
Komal <Komalpujari04@gmail.com>
2025-07-07 07:14:27 +0530

    Add JAVA CODING Folder

--- Patterns/HollowDiamond.java
+++ Patterns/HollowDiamond.java
@@ -1,71 +1,68 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class HollowDiamond {
 
     public static void printHollowDiamond(int n) {
         int mid = (n + 1) / 2;
 
-        // Upper Half
+        // Upper Half (including middle)
         for (int i = 1; i <= mid; i++) {
-            // spaces
+            // Print leading spaces
             for (int s = 1; s <= mid - i; s++) {
-                System.out.print("  ");
+                System.out.print(" ");
             }
 
-            if (i == mid) {
-                // Middle row
+            if (i == 1) {
+                System.out.print("1");
+            } else if (i == mid) {
+                // Middle row: numbers with spaces in between
                 for (int j = 1; j <= n; j++) {
-                    System.out.print(j + "  ");
+                    System.out.print(j + " ");
                 }
                 for (int j = n - 1; j >= 1; j--) {
-                    System.out.print(j + "  ");
+                    System.out.print(j + " ");
                 }
-            } else if (i == 1) {
-                System.out.print("1");
             } else {
+                // Edge numbers with spaces between
                 System.out.print("1");
-                int innerSpaces = 2 * i - 3;
-                for (int s = 1; s <= innerSpaces; s++) {
-                    System.out.print("  ");
+                for (int s = 1; s <= (2 * i - 3); s++) {
+                    System.out.print(" ");
                 }
                 System.out.print(i);
             }
-
             System.out.println();
         }
 
         // Lower Half
         for (int i = mid - 1; i >= 1; i--) {
-            // spaces
+            // Leading spaces
             for (int s = 1; s <= mid - i; s++) {
-                System.out.print("  ");
+                System.out.print(" ");
             }
 
             if (i == 1) {
                 System.out.print("1");
             } else {
                 System.out.print("1");
-                int innerSpaces = 2 * i - 3;
-                for (int s = 1; s <= innerSpaces; s++) {
-                    System.out.print("  ");
+                for (int s = 1; s <= (2 * i - 3); s++) {
+                    System.out.print(" ");
                 }
                 System.out.print(i);
             }
-
             System.out.println();
         }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter an odd number: ");
         int n = sc.nextInt();
 
-        if (n % 2 == 0 || n < 1) {
-            System.out.println("Please enter odd number.");
+        if (n % 2 == 0) {
+            System.out.println("Please enter an odd number.");
         } else {
             printHollowDiamond(n);
         }
+        sc.close();
     }
 }
